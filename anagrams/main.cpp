99d59a93fac69de684852af2473e6151fa09517c
snatchanon <62899961+natchanon-me@users.noreply.github.com>
2020-05-18 23:52:14 +0700

    Add files via upload

---
+++ anagrams/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<string>> groupAnagrams(vector<string> strs) {
+        unordered_map<string, vector<string>> m;
+        for (string s : strs) {
+            string s_temp = s;
+            sort(s_temp.begin(), s_temp.end());
+            m[s_temp].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for (auto m_elem : m) {
+            ans.push_back(m_elem.second);
+        }
+        return ans;
+    }
+
+int main()
+{
+    vector<vector<string>> an = groupAnagrams({"eat", "tea", "tan", "ate", "nat", "bat"});
+    cout << "*****" << endl;
+    for (vector<string> ss : an) {
+        for (string sss : ss) {
+            cout << sss << endl;
+        }
+        cout << "*****" << endl;
+    }
+    return 0;
+}
